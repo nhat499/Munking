8f33eb6e5fcdd6c590c0ccc8e4cdab6685f9b21d
Nhat499 <nhat499@uw.edu>
2020-12-31 20:04:04 -0800

    2nd commit

--- munking.js
+++ munking.js
@@ -2,61 +2,167 @@
 (function() {
     window.addEventListener("load", init);
     let numOfPlayer = 0;
+    let selected = false;
+    let treasure = ["one time use", "helm", "amor", "weapon", "shoe"];
     function init() {
-        let btnNewPlayer = qs("#newPlayerbtn");
-        btnNewPlayer.addEventListener("click", buttionFuction)
-
+        let newPlayerBtn = qs("#newPlayer");
+        newPlayerBtn.addEventListener("click", createNewPlayer);
     }
 
-    function buttionFuction() {
+    function createNewPlayer() {
         numOfPlayer++;
-        let newPlayer = document.createElement("div");
-        newPlayer.id = "player" + numOfPlayer;
-        newPlayer.className = "thePlayer";
-        let players = qs(".player");
-        newPlayer.textContent = "player" + numOfPlayer;
-        let player = document.createElement("text");
-        player.textContent = "P" + numOfPlayer;
-        let box1 = qs(".box");
-        box1.appendChild(player);
-        players.appendChild(newPlayer);
-        for (let i = 0; i < 2; i++) {
-            newPlayer.appendChild(createMonster());
-        }
-        for (let i = 0; i < 3; i++) {
-            newPlayer.appendChild(createTreasure());
-        }
-        newPlayer.addEventListener("click", showInfo);
-    }
+        let players = qs("#players");
+        let name = qs("#name").value;
+        let lvBox = qsa(".box")[0];
+        
 
-    function createTreasure() {
-        let treasure = document.createElement("div");
-        treasure.className = "treasure";
-        treasure.textContent = "bonus : " + getRandomInt(1,5);
-        return treasure;
+        let thePlayer = document.createElement("div");
+        thePlayer.id = name;
+        let playerName = document.createElement("h3");
+        let nameTag = document.createElement("text");
+        nameTag.textContent = name;
+        playerName.textContent = "player " + numOfPlayer + ": " + name;
+        let playerCards = document.createElement("div");
+
+        playerCards.className = "cards";
+        for (let i = 0; i < 2; i++) {
+            let card = createMonster();
+            playerCards.appendChild(card);
+            card.addEventListener("click", select);
+        }
+
+        for (let i = 0; i < 3; i++) {
+            let card = createTreasure();
+            playerCards.appendChild(card);
+            card.addEventListener("click", select);
+        }
+
+
+        lvBox.appendChild(nameTag);
+        thePlayer.appendChild(playerName);
+        thePlayer.appendChild(playerCards);
+        playerCards.classList.add("hidden");
+
+        let reveal = document.createElement("button");
+        reveal.addEventListener("click", revealftn);
+        reveal.textContent = "reveal";
+        let hide = document.createElement("button");
+        hide.textContent = "hide";
+        hide.addEventListener("click", hideftn);
+        thePlayer.append(reveal);
+        thePlayer.append(hide);
+        players.appendChild(thePlayer);
+        
     }
 
     function createMonster() {
-        let monster = document.createElement("div");
-        monster.className = "monster";
-        monster.textContent = "level : " + getRandomInt(1,15);
-        return monster;
+        let card = document.createElement("div");
+        card.className = "card";
+        let name = document.createElement("h4");
+        name.textContent = "monster-name";
+        let img = document.createElement("img");
+        img.className = "cardPic";
+        img.src = "img/monster.png";
+        let description = document.createElement("text");
+        description.textContent = "something something something something";
+        let numlv = getRandomInt(1, 15);
+        let level = document.createElement("h5");
+        level.textContent = "level: " + numlv;
+        
+        let treasure = document.createElement("h5");
+        let numTreasure = 3;
+        if ( numlv < 5) {
+            numTreasure =  1;
+        } else if (numlv >= 5 && numlv <= 10) {
+            numTreasure = 2;
+        }
+        treasure.textContent = numTreasure + " treasure";
+
+        card.append(name);
+        card.append(img);
+        card.append(description);
+        card.append(level);
+        card.append(treasure);
+        return card;
+    }
+
+    function createTreasure() {
+        let card = document.createElement("div");
+        card.className = "card";
+        let name = document.createElement("h4");
+        name.textContent = "treasure-name";
+        let img = document.createElement("img");
+        img.className = "cardPic";
+        img.src = "img/treasure.png";
+        let description = document.createElement("text");
+        description.textContent = "something something something something";
+        let bonusAmount = getRandomInt(2, 5);
+        let bonus = document.createElement("h5");
+        bonus.textContent = "+" + bonusAmount;
+        let type = document.createElement("h5");
+        type.textContent = treasure[getRandomInt(0, 5)];
+
+
+        card.append(name);
+        card.append(img);
+        card.append(description);
+        card.append(bonus);
+        card.append(type);
+        return card;
+    }
+
+    function select() {
+        if (selected == false) {
+            this.classList.add("selected");
+            selected = true;
+            let use = document.createElement("button");
+            use.id = "usebtn";
+            use.textContent = "use";
+            let cards = qs(".cards");
+            use.addEventListener("click", getRidOff);
+            cards.append(use);
+            let trade = document.createElement("button");
+            trade.id = "trade";
+            trade.textContent = "trade";
+            trade.addEventListener("click", trading);
+            cards.append(trade);
+            let tradeTo = document.createElement("input");
+            tradeTo.id = "tradeTo";
+            cards.appendChild(tradeTo);
+
+        } else if (this.classList.contains("selected")) {
+            this.classList.remove("selected");
+            selected = false;
+            qs("#usebtn").remove();
+            qs("#tradeTo").remove();
+            qs("#trade").remove();
+        }
+    }
+
+    function trading() {
+        let name = qs("#tradeTo");
+        let person = qs("#" + name.value);
+        console.log(person);
+    }
+
+    function getRidOff() {
+        qs(".selected").remove();
+        this.remove();
+        selected = false;
+    }
+
+    function revealftn() {
+        this.previousSibling.classList.remove("hidden");
+    }
+
+    function hideftn() {
+        this.previousSibling.previousSibling.classList.add("hidden");
     }
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min);
-    }
-
-    function showInfo() {
-        let infoBox = qs("#info");
-        let playerInfo = document.createElement("div");
-        let playerName = document.createElement("h4");
-        playerName.textContent = this.id + " level: ";
-        
-        playerInfo.appendChild(playerName);
-        infoBox.appendChild(playerInfo);
     }
 
     function qs(select) {
