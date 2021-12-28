e0c0bbaaa57faec19bd9aaf4fb423a52c7acd5e5
Nhat499 <nhat499@uw.edu>
2021-12-28 15:52:29 -0800

    change port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const socketio = require("socket.io");
 const io = socketio(server);
 
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
